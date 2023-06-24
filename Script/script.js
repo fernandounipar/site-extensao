5f92a114ed128cea8169cfdcf012fc96c52f8da1
fernandounipar <130415666+fernandounipar@users.noreply.github.com>
2023-06-23 22:41:33 -0300

    Arquivos

---
+++ Script/script.js
@@ -0,0 +1,77 @@
+function AbrirAda() {
+    window.location.href = "ada.html";
+  }
+
+  function AbrirHTML() {
+    window.location.href = "Html.html";
+  }  
+
+  function AbrirJavaScript() {
+    window.location.href = "javascript.html";
+  }
+
+  function AbrirCSS() {
+    window.location.href = "css.html";
+  }   
+
+  function AbrirFortran() {
+    window.location.href = "fortran.html";
+  }   
+  
+  function AbrirSmalltalk() {
+    window.location.href = "smalltalk.html";
+  }   
+
+  function AbrirC3() {
+    window.location.href = "c3.html";
+  } 
+  
+  function AbrirC() {
+    window.location.href = "c.html";
+  } 
+  
+  function AbrirC2() {
+    window.location.href = "c2.html";
+  } 
+  
+  function AbrirPHP() {
+    window.location.href = "php.html";
+  } 
+    
+  function AbrirPhyton() {
+    window.location.href = "python.html";
+  } 
+
+  function AbrirJava() {
+    window.location.href = "java.html";
+  } 
+
+  function AbrirKotlin() {
+    window.location.href = "kotlin.html";
+  } 
+
+  function AbrirRuby() {
+    window.location.href = "ruby.html";
+  } 
+
+  function AbrirR() {
+    window.location.href = "R.html";
+  } 
+
+  function AbrirSimula() {
+    window.location.href = "simula.html";
+  } 
+
+  function AbrirProlog() {
+    window.location.href = "prolog.html";
+  } 
+  
+  function AbrirPascal() {
+    window.location.href = "pascal.html";
+  } 
+    
+  function AbrirEgua() {
+    window.location.href = "egua.html";
+  } 
+
+
